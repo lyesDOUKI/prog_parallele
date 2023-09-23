fe22f03c44eeef27550361fb60164aa8795c53e6
lyesdki <lyes.douki@alumni.univ-avignon.fr>
2023-09-23 11:19:53 +0200

    exo2 part3

--- TP1/exo3.cpp
+++ TP1/exo3.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 #include<vector>
 #include<thread>
+#include<mutex>
 
 using namespace std;
 
+mutex m;
 int sumVector(vector<int> &vec)
 {
     int sum=0;
@@ -13,15 +15,18 @@ int sumVector(vector<int> &vec)
     }
     return sum;
 }
-int sum_thread(vector<int> &vec,int debut,int fin)
+void sum_thread(vector<int> &vec,int debut,int fin, int &sum)
 {
-    int sum=0;
+    //sum=0;
+
     for(int i=debut;i<fin;i++)
     {
+        m.lock();
         sum+=vec[i];
+        m.unlock();
     }
-    return sum;
 }
+
 vector<int> vec;
 int main()
 {
@@ -40,30 +45,49 @@ int main()
     cin>>nombre_threads;
     vector<thread> threads;
     int elements_par_thread=n/nombre_threads;
-    vector<int> sum_threads;
+    
+    //part 2
+    
+    /* 
+        vector<int> sum_threads;
+        for(int i=0;i<nombre_threads;i++)
+        {
+            int debut = i*elements_par_thread;
+            int fin = (i+1)*elements_par_thread;
+            threads.emplace_back(
+                thread([&sum_threads,i, debut,fin](){
+                    
+                    sum_threads.emplace_back(sum_thread(vec,debut,fin));
+                })
+            );
+        }
+        //attendre les threads
+        for(int i=0;i<nombre_threads;i++)
+        {
+            threads[i].join();
+        }
+        //sommer les sommes des threads
+        int sum=0;
+        for(int i=0;i<nombre_threads;i++)
+        {
+            sum+=sum_threads[i];
+        }
+        cout<<"version thread : la somme des elements du tableau est : "<<sum<<endl;*/
+    //part3
+    int sum=0;
     for(int i=0;i<nombre_threads;i++)
     {
         int debut = i*elements_par_thread;
         int fin = (i+1)*elements_par_thread;
         threads.emplace_back(
-            thread([&sum_threads,i, debut,fin](){
-                
-                sum_threads.emplace_back(sum_thread(vec,debut,fin));
-            })
+            thread(sum_thread,ref(vec),debut,fin, ref(sum))
         );
     }
-    //attendre les threads
     for(int i=0;i<nombre_threads;i++)
-    {
-        threads[i].join();
-    }
-    //sommer les sommes des threads
-    int sum=0;
-    for(int i=0;i<nombre_threads;i++)
-    {
-        sum+=sum_threads[i];
-    }
-    cout<<"version thread : la somme des elements du tableau est : "<<sum<<endl;
+        {
+            threads[i].join();
+        }
+    cout<<"version thread part 3 : la somme des elements du tableau est : "<<sum<<endl;
     cout<<"version classique : la somme des elements du tableau est : "<<sumVector(vec)<<endl;
     return 0;
 }
